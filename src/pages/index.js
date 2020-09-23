5a97909c408299b93ef2724f892a6c503eec501f
Philip Gaunitz <fluffen@pgaunitz.se>
2020-09-23 09:57:34 +0200

    add layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,13 @@
 import React from "react"
 
+import Layout from "../components/layout"
+
 const IndexPage = () => {
-  return <h1>Hi there</h1>
+  return (
+    <Layout>
+      I am here
+    </Layout>
+  )
 }
 
 export default IndexPage
