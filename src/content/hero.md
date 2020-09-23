1329149d2d0a5eb5f58112b01abf9a6c87774f90
Philip Gaunitz <fluffen@pgaunitz.se>
2020-09-23 12:38:16 +0200

    add hero component and md transformer pkg

---
+++ src/content/hero.md
@@ -0,0 +1,8 @@
+---
+greetings: "Hello"
+title: "Philip Gaunitz"
+subtitlePrefix: "I build and design "
+subtitleHighlight: "things for the web."
+---
+
+Full Stack Web Developer. Based in Stockholm.
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,33 @@
 import React from "react"
+import { graphql } from "gatsby"
 
+import Hero from "../components/hero"
 import Layout from "../components/layout"
 
-const IndexPage = () => {
+const IndexPage = ({data}) => {
   return (
     <Layout>
-      I am here
+      <Hero content={data.hero.edges[0].node}/>
     </Layout>
   )
 }
 
 export default IndexPage
+
+export const pageQuery = graphql`
+  {
+    hero: allMarkdownRemark {
+      edges {
+        node {
+          frontmatter {
+            title
+            greetings
+            subtitlePrefix
+            subtitleHighlight
+          }
+          rawMarkdownBody
+        }
+      }
+    }
+  }
+`
